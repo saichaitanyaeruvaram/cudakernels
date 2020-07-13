1a0763d2433d8c48845f5be4ab7ff4a77ec0eb7d
rg <rg>
2020-07-13 10:24:51 +0530

    changing blocks to 64

--- base/src/addkernel.cu
+++ base/src/addkernel.cu
@@ -27,7 +27,7 @@ __global__ void add_32(const Npp8u* src1, const Npp8u* src2, Npp8u* dst, int ste
 		return;
 	}
 
-	for (auto i = 0; i < 32; i++)
+	for (auto i = 0; i < 64; i++)
 	{
 		int offset = y * step + x + i;
 		dst[offset] = src1[offset] + src2[offset];
@@ -38,14 +38,14 @@ void launchAddKernel(const Npp8u* src1, const Npp8u* src2, Npp8u* dst, int step,
 {
 	if (method == ADD_BASIC)
 	{
-		dim3 block(32, 32);
+		dim3 block(64, 64);
 		dim3 grid((size.width + block.x - 1) / block.x, (size.height + block.y - 1) / block.y);
 		add << <grid, block, 0, stream >> > (src1, src2, dst, step, size.width, size.height);
 	}
 	else if (method == ADD_32)
-	{
-		auto width = size.width >> 5;
-		dim3 block(32, 32);
+	{ 
+		auto width = size.width >> 6;
+		dim3 block(64, 64);
 		dim3 grid((width + block.x - 1) / block.x, (size.height + block.y - 1) / block.y);
 		add_32 << <grid, block, 0, stream >> > (src1, src2, dst, step, width, size.height);
 	}
